5d1a2633ebe5da4c979521abd003bd669a435377
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-05 21:21:25 -0400

    Added: Default Params for api

---
+++ Default.ts
@@ -0,0 +1,11 @@
+import { default_Params_Type } from "./Jwt.Types";
+//Where private keys  and constants will be manifested
+/*HI */
+ export const default_Params: default_Params_Type = {
+    port: 3000,//LocalHost Testing Port
+    db_Key: "Data Key Here",
+    jwt_Private_Key: "",
+    jwt_Public_Key: "",
+    jwt_Access_Token_TTL: "",
+    jwt_Refresh_Token_TTL: "",
+}//default object to hold key parameters

e6b17e5a655485a806e48fcafc85ad6e078bdbe2
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-05 21:26:56 -0400

    Added: Comments for clairty

--- Default.ts
+++ Default.ts
@@ -1,11 +1,11 @@
 import { default_Params_Type } from "./Jwt.Types";
 //Where private keys  and constants will be manifested
-/*HI */
+
  export const default_Params: default_Params_Type = {
     port: 3000,//LocalHost Testing Port
-    db_Key: "Data Key Here",
-    jwt_Private_Key: "",
-    jwt_Public_Key: "",
-    jwt_Access_Token_TTL: "",
-    jwt_Refresh_Token_TTL: "",
+    db_Key: "Data Key Here",//MongoDb Connection Key here
+    jwt_Private_Key: "",//Jwt hash Private Key
+    jwt_Public_Key: "",//Jwt hash Public key
+    jwt_Access_Token_TTL: "",//Jwt Access Token Time to Libe (expire)
+    jwt_Refresh_Token_TTL: "",//Jwt Refresh Token Time to live (expire)
 }//default object to hold key parameters

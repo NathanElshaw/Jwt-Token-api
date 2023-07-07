4124fe12631fde0eff5d7994105fd7ab99ec562d
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-06 21:11:43 -0400

    Added: get jwt api to route

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,7 +1,8 @@
 import { Express } from "express";
+import session_Handler from "./Controller/Jwt.Controller";
 
 const Routes = (app: Express) => {
-  app.get("/api");
+  app.get("/api", session_Handler.Create);
 };
 
 export default Routes;

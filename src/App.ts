66686f8914594b88e17858cfb6cea2173e1b65aa
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-06 20:59:07 -0400

    Styling: formmated

---
+++ src/App.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { default_Params } from "../Default";
+import routes from "./Routes";
+import db_Connection from "./Provider/Db.Provider";
+
+const App = express();
+const port: number = default_Params.port;
+
+App.listen(port, async () => {
+  await db_Connection();
+  console.log(`Api Running on Port: ${port}`);
+  routes(App);
+});

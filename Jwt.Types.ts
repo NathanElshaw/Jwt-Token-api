218134d751856ee43a810457bd290d2ec6d485dd
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-06 21:05:42 -0400

    Added: global types file

---
+++ Jwt.Types.ts
@@ -0,0 +1,8 @@
+export interface default_Params_Type {
+  port: number;
+  db_Key: string;
+  jwt_Private_Key: string;
+  jwt_Public_Key: string;
+  jwt_Access_Token_TTL: string;
+  jwt_Refresh_Token_TTL: string;
+}

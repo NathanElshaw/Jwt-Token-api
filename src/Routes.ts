7078227951b1c6955bf85ff9015e0ef70ab03066
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-06 20:47:59 -0400

    Added: temp routes

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,7 +1,16 @@
 import { Express } from "express";
+import session_Handler from "./Controller/Jwt.Controller";
 
 //Handles all of the api routes in the api
-function routes(App: Express){
-
+function routes(app: Express) {
+  app.get("/api", (req: any, res: any) => {
+    try {
+      console.log(req.socket.remoteAddress);
+      return res.send("Done");
+    } catch (e: any) {
+      return e.message;
+    }
+  });
+  app.get("/api/session", session_Handler.Create);
 }
-export default routes
+export default routes;

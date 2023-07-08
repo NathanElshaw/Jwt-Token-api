2c7fbf082902cbbc0e5ec1f5442e9ded07dd19c8
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-07 23:36:52 -0400

    Added: temp check for jwt refresh

--- src/Routes.ts
+++ src/Routes.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import session_Handler from "./Controller/Jwt.Controller";
 
 const Routes = (app: Express) => {
-  app.get("/api", session_Handler.Create);
+  app.get("/api", session_Handler.Check_Refresh);
 };
 
 export default Routes;

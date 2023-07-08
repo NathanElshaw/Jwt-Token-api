e9020e72a5783b9230b0de740e713396ec18b8a1
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-08 15:05:19 -0400

    Added: comments for readablility

--- src/Provider/Db.Provider.ts
+++ src/Provider/Db.Provider.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { default_Params } from "../../Default/Default";
 
-mongoose.set("strictQuery", false);
+mongoose.set<"strictQuery">("strictQuery", false);
 
+//Connect to mongoDb with this functions
 export default async function db_Connection() {
   try {
-    await mongoose.connect(default_Params.db_Key);
+    await mongoose.connect(default_Params.db_Key); // use default params db_key to connect
   } catch (e: any) {
     console.log(e.message);
   }

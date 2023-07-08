46320a30a52581add74bbc210605f9b6264e7bb3
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-08 15:06:35 -0400

    Added: user_Agent and Access_Ip to payload

--- src/Service/Jwt.Service.ts
+++ src/Service/Jwt.Service.ts
@@ -4,8 +4,12 @@ const session_Service = {
   /*Creates a Mongodb Document to use to store session */
   Create: async (userId: string, userAgent: string, access_Ip: string) => {
     try {
-      const session = { user: userId }; //created a mongodb document with inputs
-
+      const session = {
+        user: userId,
+        userAgent: userAgent,
+        access_Ip: access_Ip,
+      }; //created a mongodb document with inputs
+      //put create doucment async here for mongodb
       return session; //return create as a json to be used in jwt
     } catch (e: any) {
       return e;

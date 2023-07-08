ffff5476dba9021ee9c8f408af50add79d178715
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-08 15:04:33 -0400

    Added: check refresh property to jwt provider

--- src/Provider/Jwt.Provider.ts
+++ src/Provider/Jwt.Provider.ts
@@ -1,6 +1,8 @@
-import jwt, { decode, verify } from "jsonwebtoken";
-import { default_Params } from "../../Default/Default";
+import jwt, { sign } from "jsonwebtoken";
+import { get } from "lodash";
 import { NextFunction } from "express";
+import session_Service from "../Service/Jwt.Service";
+import { default_Params } from "../../Default/Default";
 
 const jwt_Provider = {
   sign: (
@@ -20,9 +22,9 @@ const jwt_Provider = {
 
   verify(token: string, keyName: string) {
     //gets type of key for a buffer constructor used to verify jwt
-    const public_Key = Buffer.from(keyName, "base64").toString("ascii");
+    const signing_Key = Buffer.from(keyName, "base64").toString("ascii");
     try {
-      const decoded = jwt.verify(token, public_Key);
+      const decoded = jwt.verify(token, signing_Key);
       return { valid: true, expired: false, decoded }; //Returns valid jwt with user data
     } catch (e: any) {
       console.error(e);
@@ -35,32 +37,57 @@ const jwt_Provider = {
   },
 
   refresh(
-    access_Token: string,
-    refresh_Token: string,
-    keyName: string,
-    refresh_Options: object,
-    next: NextFunction
+    access_Token: string, //current access token
+    refresh_Token: string, //current refresh token
+    keyName: string //key name for making signing key
   ) {
-    const public_Key = Buffer.from(keyName, "base64").toString("ascii");
+    const signing_Key = Buffer.from(keyName, "base64").toString("ascii"); //creates signing key with private or public key
     try {
-      const decoded = jwt.verify(access_Token, public_Key);
+      const access_Decoded = jwt.verify(access_Token, signing_Key); //decodes the current access token to check if its valid
       return {
-        decoded,
+        access_Decoded, //if valid will return jwt data
       };
     } catch (e: any) {
       if (e.message === "jwt expired") {
         try {
-          const refresh_Decoded = jwt.verify(refresh_Token, public_Key);
-          if (refresh_Decoded)
-            jwt.sign(refresh_Decoded, public_Key, {
-              ...refresh_Options,
-              algorithm: "RS256",
-            });
+          const refresh_Decoded = jwt.verify(refresh_Token, signing_Key); //on a invaild access token will check refresh token to see if its valid
+          //if valid then will reissue a new refresh and access token
+          if (refresh_Decoded) {
+            const new_Access_Token = jwt.sign(
+              { payload: get(refresh_Decoded, "payload") },
+              signing_Key,
+              {
+                expiresIn: default_Params.jwt_Access_Token_TTL,
+                algorithm: "RS256",
+              }
+            );
+            const new_Refresh_Token = jwt.sign(
+              { payload: get(refresh_Decoded, "payload") },
+              signing_Key,
+              {
+                expiresIn: default_Params.jwt_Refresh_Token_TTL, // defined in default params i.e "15m" = 15 minutes
+                algorithm: "RS256",
+              }
+            );
+
+            return {
+              new_Access_Token: new_Access_Token,
+              new_Refresh_Token: new_Refresh_Token,
+            };
+          }
         } catch (e: any) {
           console.error(e.message);
           return e.message;
         }
       }
+      return e.message;
+    }
+  },
+
+  delete_session: async () => {
+    try {
+      //invalidate session in db then in client
+    } catch (e: any) {
       return e.message;
     }
   },

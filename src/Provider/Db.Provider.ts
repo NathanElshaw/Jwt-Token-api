bf998749ee2f3841793088f9e4ba5b70fc4cee2a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-07 23:35:55 -0400

    Added: correct import for default

--- src/Provider/Db.Provider.ts
+++ src/Provider/Db.Provider.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose"
-import { default_Params } from "../../Default";
+import mongoose from "mongoose";
+import { default_Params } from "../../Default/Default";
 
 mongoose.set("strictQuery", false);
 
-export default async function db_Connection(){
-    try{
-        await mongoose.connect(default_Params.db_Key)
-    }catch(e: any){
-        console.log(e.message)
-    }
+export default async function db_Connection() {
+  try {
+    await mongoose.connect(default_Params.db_Key);
+  } catch (e: any) {
+    console.log(e.message);
+  }
 }

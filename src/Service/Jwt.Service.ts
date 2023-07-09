3758d95065fe0b5842827fe6a1e1814526a9bf48
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-09 07:57:57 -0600

    Added: delete session & sessid to payload

--- src/Service/Jwt.Service.ts
+++ src/Service/Jwt.Service.ts
@@ -1,4 +1,4 @@
-import Session_Model from "../Models/Sessions.model";
+import { Response } from "express";
 
 const session_Service = {
   /*Creates a Mongodb Document to use to store session */
@@ -8,11 +8,22 @@ const session_Service = {
         user: userId,
         userAgent: userAgent,
         access_Ip: access_Ip,
+        _id: "123423293432ew",
       }; //created a mongodb document with inputs
       //put create doucment async here for mongodb
       return session; //return create as a json to be used in jwt
     } catch (e: any) {
-      return e;
+      console.error({ "Session Create:": e.message });
+      return e.message;
+    }
+  },
+
+  Delete_Session: async (session_Id: any, res: Response) => {
+    try {
+      console.log(session_Id.session);
+    } catch (e: any) {
+      console.error({ "Session Delete:": e.message });
+      return e.message;
     }
   },
 };

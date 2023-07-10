3680f60b693df56c06ec60f536d81cdd48b7c2a1
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-09 18:10:02 -0600

    Added: get session to routes

--- src/Routes.ts
+++ src/Routes.ts
@@ -2,7 +2,8 @@ import { Express } from "express";
 import session_Handler from "./Controller/Jwt.Controller";
 
 const Routes = (app: Express) => {
-  app.get("/api", session_Handler.Check_Refresh, session_Handler.Create);
+  app.get("/api", session_Handler.Create);
+  app.get("/api/get", session_Handler.Get_Session);
   app.get("/api/delete", session_Handler.Delete_Session);
 };
 

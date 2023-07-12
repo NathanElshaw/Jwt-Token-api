f8066639b742de6cc133a1dabd802ba2f2164c10
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-11 21:04:34 -0600

    Added: changed object id to string and changed doc db location

--- src/Models/Sessions.model.ts
+++ src/Models/Sessions.model.ts
@@ -2,26 +2,31 @@ import mongoose from "mongoose";
 
 //declaring db model for doucment insertions
 export interface Session_Document extends mongoose.Document {
-    user: mongoose.Types.ObjectId;
-    valid: boolean;
-    userAgent: string;
-    accessIp: string;
-    createdAt: Date;
-    updatedAt: Date;
+  user: string;
+  valid: boolean;
+  userAgent: string;
+  accessIp: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 //creating a schema for allowed params to be added to document
-const session_Schema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, required: true},
-    valid: {type: Boolean, require: true, default: true},
-    userAgent: {type: String, require: true },
-    accessIp: {type: String, require: true}
-},
- {
-    timestamps: true
- })
+const session_Schema = new mongoose.Schema(
+  {
+    user: { type: String, required: true },
+    valid: { type: Boolean, require: true, default: true },
+    userAgent: { type: String, require: true },
+    accessIp: { type: String, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
- //Session model to use to pass mongoDb fucntions through to create, patch or delete documents
- const Session_Model = mongoose.model<Session_Document>("Session", session_Schema)
+//Session model to use to pass mongoDb fucntions through to create, patch or delete documents
+const Session_Model = mongoose.model<Session_Document>(
+  "Jwt_Sessions",
+  session_Schema
+);
 
- export default Session_Model
+export default Session_Model;

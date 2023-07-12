b16a8b3203a70ad1ecaa07a9cd097fd635559fb0
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-11 21:02:38 -0600

    Added: jwt_data_type

--- Jwt.Types.ts
+++ Jwt.Types.ts
@@ -6,3 +6,12 @@ export interface default_Params_Type {
   jwt_Access_Token_TTL: string;
   jwt_Refresh_Token_TTL: string;
 }
+
+export interface jwt_Payload_Type {
+  payload: {
+    _id: string;
+    session: string;
+  };
+  iat: string;
+  exp: string;
+}

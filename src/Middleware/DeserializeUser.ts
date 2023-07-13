9beb59a7e0e2d7c7e62eaf49397cc920128a357b
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-13 09:02:48 -0700

    Added: push jwt data to local response

--- src/Middleware/DeserializeUser.ts
+++ src/Middleware/DeserializeUser.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import session_Handler from "../Controller/Jwt.Controller";
+import jwt_Provider from "../Provider/Jwt.Provider";
+import { default_Params } from "../../Default/Default";
 
 async function DeserializeUser(
   req: Request,
@@ -7,7 +9,19 @@ async function DeserializeUser(
   next: NextFunction
 ) {
   try {
-    return session_Handler.Check_Refresh(req, res, next); //Checks tokens else will push next function if valid
+    const access_Token = req.cookies.access_Token;
+    const refresh_Token = req.cookies.refresh_Token;
+
+    if (access_Token && refresh_Token) {
+      const Check_Refresh = await session_Handler.Check_Refresh(req, res, next);
+      const { decoded } = jwt_Provider.verify(
+        req.cookies.access_Token,
+        default_Params.jwt_Private_Key
+      );
+      if (decoded) res.locals.user = decoded;
+      next();
+    }
+    next();
   } catch (e: any) {
     console.error({ "Desrialize-User:": e.message });
     return e.message;
